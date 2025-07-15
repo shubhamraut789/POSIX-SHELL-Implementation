e8e1f93a81547fc18456067579d1f96f51fe0827
Shubham Raut <66682400+shubhamraut789@users.noreply.github.com>
2025-07-15 20:52:52 +0530

    Updated history.cpp

--- history.cpp
+++ history.cpp
@@ -1,93 +1,71 @@
 #include <iostream>
 #include <unistd.h>
-#include <syscall.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
-#include <algorithm>
-#include <map>
-#include <errno.h>
+#include <string>
 #include <vector>
 #include <fstream>
-#include <string>
+#include <deque>
 
 using namespace std;
 
-vector<string> history_comm;
+deque<string> history_comm;  // use deque for efficient pop from front
 
-void add_to_history(const string& command,const string& filename) {
-    
-    if(history_comm.size() >= 20) {
-        history_comm.erase(history_comm.begin());
-    }
-
-    //history_comm.push_back(command);
-
-    int fd = open(filename.c_str(), O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    if(fd == -1){
-        perror("Error while opening history.txt for writing");
+// Called once at startup
+void load_history_from_file(const string& filename) {
+    ifstream infile(filename);
+    if (!infile.is_open()) {
+        // Create empty file if doesn't exist
+        ofstream outfile(filename);
+        outfile.close();
         return;
     }
 
-    string data = command + '\n';
-    if(write(fd, data.c_str(), data.size()) == -1) {
-        perror("Error while writing to history.txt");
-    }
-
-    ifstream file(filename);
-    
-    if(!file.is_open()) {
-        perror("Error while opening file history.txt");
-    }
-    
     string line;
+    while (getline(infile, line)) {
+        if (!line.empty()) {
+            history_comm.push_back(line);
+            if (history_comm.size() > 20) {
+                history_comm.pop_front();
+            }
+        }
+    }
+    infile.close();
+}
 
-    while(getline(file, line)) {
-        history_comm.push_back(line);
+// Called each time a command is entered
+void add_to_history(const string& command, const string& filename) {
+    if (command.empty()) return;
+
+    history_comm.push_back(command);
+    if (history_comm.size() > 20) {
+        history_comm.pop_front();
     }
 
+    // Rewrite file with updated 20 commands
+    ofstream outfile(filename, ios::trunc);
+    for (const string& cmd : history_comm) {
+        outfile << cmd << '\n';
+    }
+    outfile.close();
 }
 
-void call_history(){
-
-        if(history_comm.size() >= 20){
-            history_comm.erase(history_comm.begin());
-        }
-        int start;
-
-        if(history_comm.size() > 10){
-            start = history_comm.size() - 10;
-        }else start = 0;
-
-        for(int i = start; i < history_comm.size(); i++){
-            cout << history_comm[i] << '\n';
-        }
-
+void call_history() {
+    int start = max(0, (int)history_comm.size() - 10);
+    for (int i = start; i < history_comm.size(); ++i) {
+        cout << history_comm[i] << '\n';
+    }
 }
 
-void call_history1(int num){
+void call_history1(int num) {
+    if (num < 0) {
+        cout << "history: number must be non-negative\n";
+        return;
+    }
 
-        if(num < 0){
-            cout<<"history num is negative"<<'\n';
-            return ;
-        }
+    int cnt = min(num, (int)history_comm.size());
+    int start = history_comm.size() - cnt;
 
-        while(history_comm.size() > 20){
-            history_comm.erase(history_comm.begin());
-        }
-        int start;
-        int cnt;
-        if(num == -1) cnt = 10;
-        else cnt = num;
-
-        if(cnt > history_comm.size()) cnt = history_comm.size();
-
-        start = history_comm.size() - cnt;
-
-        for(int i = start; i < history_comm.size(); i++){
-            cout << history_comm[i] << '\n';
-        }
-        history_comm.clear();
-
+    for (int i = start; i < history_comm.size(); ++i) {
+        cout << history_comm[i] << '\n';
+    }
 }
